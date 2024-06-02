5398db6c5728a310a10e48e55c1f936295d40c08
lect-ad <64366181+lect-ad@users.noreply.github.com>
2024-06-02 19:28:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')) 
 #fruit_list = my_dataframe.select(col('FRUIT_NAME'), col('SEARCH_ON')) 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(data=pd_df, use_container_width=True)
+#st.dataframe(data=pd_df, use_container_width=True)
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
@@ -34,10 +34,10 @@ if ingredients_list:
     st.text(ingredients_string)
     for fruit_chosen in ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{search_on}")
         st.subheader(f'{fruit_chosen} Nutrition Information:')
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True).select(col("nutritions")
 
     if customer_name:
         submit_button = st.button('Submit Order')
