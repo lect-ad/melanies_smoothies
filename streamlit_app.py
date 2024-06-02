4f651396771fd41c7bb616db818d8d65155629be
lect-ad <64366181+lect-ad@users.noreply.github.com>
2024-06-02 19:29:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ if ingredients_list:
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{search_on}")
         st.subheader(f'{fruit_chosen} Nutrition Information:')
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True).select(col("nutritions")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True).select(col("nutritions"))
 
     if customer_name:
         submit_button = st.button('Submit Order')
